5645e9b7d3cd22202ee1b19d104d5f0f054614b5
root <root@akipc.localdomain>
2026-09-15 17:13:11 +0900

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        // ダミーデータを作成（本来はデータベースから取得）
+        $products = collect([
+            (object) [
+                'name' => 'ノートパソコン',
+                'category' => '電子機器',
+                'price' => 89800,
+                'in_stock' => true,
+            ],
+            (object) [
+                'name' => 'ワイヤレスマウス',
+                'category' => '周辺機器',
+                'price' => 3980,
+                'in_stock' => true,
+            ],
+            (object) [
+                'name' => 'USBハブ',
+                'category' => '周辺機器',
+                'price' => 2480,
+                'in_stock' => false,
+            ],
+        ]);
+
+        return view('products.index', [
+            'pageTitle' => '商品一覧',
+            'products' => $products,
+            'lastUpdated' => '2024年1月1日 12:00',
+        ]);
+    }
+}
